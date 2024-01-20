eb09a71ac85723786302120b163592f8218c0cb9
Dharani Dharan <dharani>
2024-01-20 20:33:30 +0530

    updated last string <print c >

--- java.java
+++ java.java
@@ -1,10 +1,11 @@
 package org.tcs;
 
 public class add {
-    public static void main ( String[]args)
+    public static void main ( String[]args) { 
     int a=10;
     int b=30;
     int c;
     c=a+b;
     System.out.println(c);
-}
+
+    }}
